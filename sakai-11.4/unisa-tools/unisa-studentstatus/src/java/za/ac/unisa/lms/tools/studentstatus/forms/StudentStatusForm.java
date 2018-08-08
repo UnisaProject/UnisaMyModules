e37c85b91e1dff930a8530917b93cbdceb0b0a42
pretoj <pretoj@unisa.ac.za>
2018-08-08 11:44:17 +0200

    correct version

--- sakai-11.4/unisa-tools/unisa-studentstatus/src/java/za/ac/unisa/lms/tools/studentstatus/forms/StudentStatusForm.java
+++ sakai-11.4/unisa-tools/unisa-studentstatus/src/java/za/ac/unisa/lms/tools/studentstatus/forms/StudentStatusForm.java
@@ -24,7 +24,7 @@ public class StudentStatusForm extends ValidatorActionForm {
 	// --------------------------------------------------------- Instance Variables
 
 	private static final long serialVersionUID = 1L;
-	private static final String version = "2018001b";
+	private static final String version = "2018002a";
 	public static Log log = LogFactory.getLog(StudentStatusAction.class);
 	
 	private String applyType = "";
