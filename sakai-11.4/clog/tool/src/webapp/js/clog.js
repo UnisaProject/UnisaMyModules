65ede5e91af3b3e821affd877ce443b5a592bbf1
Sphamandla <masiksj@unisa.ac.za>
2018-10-24 12:14:14 +0200

    Increased timeout on Clog

--- sakai-11.4/clog/tool/src/webapp/js/clog.js
+++ sakai-11.4/clog/tool/src/webapp/js/clog.js
@@ -29,8 +29,8 @@ clog.sortByComments = clog.SORT_COMMENTS_DOWN;
 // Sorting keys end
 //
 clog.LOCAL_STORAGE_KEY = 'clog';
-//clog.AJAX_TIMEOUT = 5000;
-clog.AJAX_TIMEOUT = 10000;
+clog.AJAX_TIMEOUT = 5000;
+//clog.AJAX_TIMEOUT = 10000;
 
 
 clog.states = {
